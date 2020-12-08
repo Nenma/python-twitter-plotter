0ff58531fb2f8f776e68c1ba03a4cf4c4a11c4d2
Cristi Rosu <cristi.epic420@gmail.com>
2020-12-08 16:25:32 +0200

    added key security

--- crawler.py
+++ crawler.py
@@ -1,21 +1,32 @@
 import tweepy as tw
 import json
+import re
 
-CONSUMER_KEY = 'pDyDjcwzNtFJHvAXHNJuhoPXk'
-CONSUMER_SECRET_KEY = 'gy6edffCSdyrp1JY2WGoimrnS1HZ4ugJZxGaFBZLwA3c4O3kwj'
-ACCESS_TOKEN = '1222796143-zlU5KHz0epkhpxQ3UAGxXgfaF0SFV2pdpHZs41C'
-ACCESS_SECRET_TOKEN = 'W4X23DV12AJIpIVYzWfMTnlxKZbCcTj0d9KMIKHdXTxfb'
+keys_json = open('keys.json', 'r').read()
+keys = json.loads(keys_json)
+
+CONSUMER_KEY = keys['consumer_key']
+CONSUMER_SECRET_KEY = keys['consumer_secret_key']
+ACCESS_TOKEN = keys['access_token']
+ACCESS_SECRET_TOKEN = keys['access_secret_token']
 
 auth = tw.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET_KEY)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET_TOKEN)
 api = tw.API(auth, wait_on_rate_limit=True)
 
 
-def get_tweets():
+def get_tweets(to_search, count):
     tweetsList = list()
-    # filtering for the specified tweets
+    pattern = re.compile('^[a-zA-Z]+$')
+
+    for tweet in tw.Cursor(api.search, q=to_search, lang='en', tweet_mode='extended', since='2020-12-01').items(count):
+        if pattern.match(tweet._json['user']['location']) != None:
+            tweetsList.append(tweet._json['full_text'])
+            print(tweet._json['user']['location'])
     return tweetsList
 
 
 if __name__ == '__main__':
-    get_tweets()
+    for tweet in get_tweets('#news', 30):
+        print(tweet)
+        print()
