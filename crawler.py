bae3ed959659169327232a1429028f80db6b3c1c
Cristi Rosu <cristi.epic420@gmail.com>
2020-12-16 13:43:06 +0200

    improved geolocation searching

--- crawler.py
+++ crawler.py
@@ -1,6 +1,5 @@
 import tweepy as tw
 import json
-import re
 
 keys_json = open('config/keys.json', 'r').read()
 keys = json.loads(keys_json)
@@ -17,16 +16,18 @@ api = tw.API(auth, wait_on_rate_limit=True)
 
 def get_tweets(to_search, count):
     tweetsList = list()
-    pattern = re.compile('^[a-zA-Z]+$')
 
-    for tweet in tw.Cursor(api.search, q=to_search, lang='en', tweet_mode='extended', since='2020-12-01').items(count):
-        if pattern.match(tweet._json['user']['location']) != None:
+    for tweet in tw.Cursor(api.search, q=to_search, geocode='51.51702,-0.12701,10000km', lang='en', tweet_mode='extended', since='2020-12-01').items(count):
+        if tweet._json['user']['location'] == '':
+            pair = ('California', tweet._json['full_text'])
+        else:
             pair = (tweet._json['user']['location'], tweet._json['full_text'])
-            tweetsList.append(pair)
+        tweetsList.append(pair)
+        # print(tweet._json['user']['location'])
     return tweetsList
 
 
 if __name__ == '__main__':
-    for tweet in get_tweets('#news', 30):
+    for tweet in get_tweets('#romania', 10):
         print(tweet)
         print()
--- plotter.py
+++ plotter.py
@@ -1,47 +1,77 @@
 import plotly.graph_objects as go
 from geopy.geocoders import Nominatim
+import random
 import crawler
 
-mapbox_access_token = open("config/.mapbox_token").read()
-geolocator = Nominatim(user_agent='TwitterPlotter')
-tweets = crawler.get_tweets('#virus', 30)
 
-latitudes = list()
-longitudes = list()
-texts = list()
+def get_tweets_locations(to_search, count):
+    tweets = crawler.get_tweets(to_search, count)
+    geolocator = Nominatim(user_agent='TwitterPlotter')
 
-# for tweet in tweets:
-#     print(tweet[0])
+    latitudes = list()
+    longitudes = list()
+    texts = list()
 
-for tweet in tweets:
-    location = geolocator.geocode(tweet[0])
-    latitudes.append(location.latitude)
-    longitudes.append(location.longitude)
-    texts.append(tweet[0] + ': ' + tweet[1])
+    none_found = 0
+    for tweet in tweets:
+        location = geolocator.geocode(tweet[0])
+        already_located = False
 
-fig = go.Figure(go.Scattermapbox(
-        lat=latitudes,
-        lon=longitudes,
-        mode='markers',
-        marker=go.scattermapbox.Marker(
-            size=9
+        if location is not None:
+            for i in range(len(latitudes)):
+                if location.latitude == latitudes[i] and location.longitude == longitudes[i]:
+                    already_located = True
+                    latitudes.append(location.latitude + random.uniform(-0.005, 0.005))
+                    longitudes.append(location.longitude + random.uniform(-0.005, 0.005))
+                    texts.append(tweet[0] + ': ' + tweet[1])
+                    break
+
+            if not already_located:
+                latitudes.append(location.latitude)
+                longitudes.append(location.longitude)
+                texts.append(tweet[0] + ': ' + tweet[1])
+        else:
+            none_found += 1
+
+    return latitudes, longitudes, texts, none_found
+
+
+def generate_html_map(to_search, count):
+    mapbox_access_token = open('config/.mapbox_token').read()
+    latitudes, longitudes, texts, none_found = get_tweets_locations(to_search, count)
+
+    if none_found != 0:
+        print('Could not locate', none_found, 'out of', count, 'locations')
+    else:
+        print('Successfully located all', count,'locations!')
+
+    fig = go.Figure(go.Scattermapbox(
+            lat=latitudes,
+            lon=longitudes,
+            mode='markers',
+            marker=go.scattermapbox.Marker(
+                size=9
+            ),
+            text=texts,
+        ))
+
+    fig.update_layout(
+        autosize=True,
+        hovermode='closest',
+        mapbox=dict(
+            accesstoken=mapbox_access_token,
+            bearing=0,
+            center=dict(
+                lat=51.507351,
+                lon=-0.127758
+            ),
+            pitch=0,
+            zoom=2
         ),
-        text=texts,
-    ))
+    )
 
-fig.update_layout(
-    autosize=True,
-    hovermode='closest',
-    mapbox=dict(
-        accesstoken=mapbox_access_token,
-        bearing=0,
-        center=dict(
-            lat=51.507351,
-            lon=-0.127758
-        ),
-        pitch=0,
-        zoom=2
-    ),
-)
+    fig.write_html('figure.html', auto_open=True)
 
-fig.write_html('figure.html', auto_open=True)
+
+if __name__ == '__main__':
+    generate_html_map('#antivax', 30)
